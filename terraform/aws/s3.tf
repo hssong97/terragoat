36d8cc9b35753b2fb39542fdd478d7b501066d50
hssong97 <78056306+hssong97@users.noreply.github.com>
2022-09-14 08:13:52 -0700

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -19,7 +19,10 @@ resource "aws_s3_bucket" "data" {
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
 }
-
+resource "aws_ebs_volume" "v" {
+  size = 40
+  encrypted = false
+}
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
